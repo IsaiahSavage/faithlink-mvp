773b437f6ccc8869356cf29cd083c6cb811eba5b
Isaiah Savage <savageit16@gmail.com>
2024-01-29 23:26:56 -0500

    implement login streak (#18)
    Signed-off-by: Isaiah Savage <savageit16@gmail.com>

--- src/components/LoginTracker.js
+++ src/components/LoginTracker.js
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { Feather } from '@expo/vector-icons';
+import { useUserContext } from '../contexts/UserContext';
+import { FIRESTORE_DB } from '../../firebase/firebaseConfig';
+import { doc, updateDoc, Timestamp } from 'firebase/firestore';
 
+/**
+ * LoginTracker component tracks the user's login streak and displays it on the screen.
+ * It updates the login streak in the user's data and renders the login streak UI.
+ */
 const LoginTracker = () => {
+  const { state, dispatch } = useUserContext();
+
+  /**
+   * Sets the login streak for a user based on the last login date and current streak.
+   * @param {Object} lastLogin - The last login date object.
+   * @param {number} loginStreak - The current login streak.
+   * @returns {Promise<void>} - A promise that resolves when the login streak is updated.
+   */
+  const setLoginStreak = async (lastLogin, loginStreak) => {
+    if (!lastLogin || !loginStreak) return;
+    try {
+      const today = new Date(Date.now());
+      const lastLoginDate = new Date(lastLogin.seconds * 1000);
+      const yesterday = new Date(today - 24 * 60 * 60 * 1000);
+
+      if (lastLoginDate.toDateString() === today.toDateString()) {
+        return;
+      } else if (lastLoginDate.getDate() < yesterday.getDate()) {
+        dispatch({ type: 'SET_LOGIN_STREAK', payload: 1 });
+        dispatch({
+          type: 'SET_LAST_LOGIN_DATE',
+          payload: Timestamp.fromDate(today),
+        });
+        const userRef = doc(FIRESTORE_DB, 'users', state.userID);
+        await updateDoc(userRef, {
+          loginStreak: 1,
+          lastLoginDate: Timestamp.fromDate(today),
+        });
+      } else if (lastLoginDate.getDate() < today.getDate()) {
+        dispatch({ type: 'SET_LOGIN_STREAK', payload: loginStreak + 1 });
+        dispatch({
+          type: 'SET_LAST_LOGIN_DATE',
+          payload: Timestamp.fromDate(today),
+        });
+        const userRef = doc(FIRESTORE_DB, 'users', state.userID);
+        await updateDoc(userRef, {
+          loginStreak: loginStreak + 1,
+          lastLoginDate: Timestamp.fromDate(today),
+        });
+      }
+    } catch (error) {
+      console.log('Error updating login streak: ' + error.message);
+    }
+  };
+
+  useEffect(() => {
+    setLoginStreak(state.userData.lastLoginDate, state.userData.loginStreak);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={[styles.loginText, styles.loginTitle]}>Weekly Logins</Text>
-      <View style={styles.loginCheckContainer}>
-        {/* TODO: prop for complete (black) / incomplete (gray) */}
+      <Text style={[styles.loginText, styles.loginTitle]}>Login Streak</Text>
+      {/* <View style={styles.loginCheckContainer}>
         <Feather name={'check'} size={35} color={'#337AB7'} />
         <Feather name={'check'} size={35} color={'#337AB7'} />
         <Feather name={'check'} size={35} color={'#337AB7'} />
@@ -16,9 +70,10 @@ const LoginTracker = () => {
         <Feather name={'check'} size={35} color={'#337AB7'} />
         <Feather name={'check'} size={35} color={'#337AB7'} />
         <Feather name={'check'} size={35} color={'gray'} />
-      </View>
-      {/* TODO: prop for streak number */}
-      <Text style={styles.loginText}>Streak: 128</Text>
+      </View> */}
+      <Text style={(styles.loginText, styles.loginBody)}>
+        {state.userData.loginStreak}
+      </Text>
     </View>
   );
 };
@@ -33,13 +88,17 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   loginText: {
-    marginLeft: 10,
+    marginHorizontal: 10,
     fontSize: 20,
     marginVertical: 5,
     color: '#002857',
   },
   loginTitle: {
     fontWeight: '600',
+  },
+  loginBody: {
+    fontSize: 60,
+    marginBottom: 10,
   },
   loginCheckContainer: {
     marginHorizontal: 20,
