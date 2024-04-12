f2632ace8b4261ccbfd91e8b85c54f4dc9e7d635
HyperbolicInt <ginlawhon@mail.mvnu.edu>
2024-04-12 09:32:53 -0400

    trying to make morescreen test work

--- src/screens/__tests__/MoreScreen.test.js
+++ src/screens/__tests__/MoreScreen.test.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
+
 import MoreScreen from '../MoreScreen';
 
 
 //import necessary mock data to make navigation work
 //probably the button component
-
+jest.mock('useNavigation()', () => ({
+      state: {
+        // Mocked navigation data
+        userID: 1,
+        userData: {
+          id: 1,
+          first: 'John',
+          last: 'Doe',
+          email: 'john.doe@example.com',
+        },
+      },
+      // Mocked function
+      dispatch: jest.fn()
+  }));
 
 describe('More Screen',() =>{
 
